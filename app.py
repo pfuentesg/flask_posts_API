98bbe2ad7469bc6321538bd900fce11cac16ef99
Pablo.Fuentes <pablo.fuentes@intelygez.com>
2018-11-09 18:43:54 +0100

    refactor holi resource to posts (nly returns a simple string)

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 from http_instance.Server import app
-from Resources.resources import Holi
+from Resources.posts import Posts
 from http_instance.api import MainApi
 
 
 
 def add_resources():
     api = MainApi(app)
-    api.add_source(Holi, '/holi')
+    api.add_source(Posts, '/')
 
 
 
