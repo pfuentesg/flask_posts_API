c0e942fe5f3409218d9322c1fb23660b8c5a5e38
Pablo.Fuentes <pablo.fuentes@intelygez.com>
2018-11-09 18:06:45 +0100

    working api with class MAinAPi

--- api.py
+++ api.py
@@ -1,9 +1,17 @@
-from flask_restful import  Api
-from flask import  Blueprint
-from resources import  Holi
-from app import  app
-api = Api(app)
-api.add_resource(Holi, '/holi')
+from flask_restful import Api
+from flask import Blueprint
+from resources import Holi
+from app import app
 
-app.run(
 
+class MainApi():
+    def __init__(self, app):
+        self.api = Api(app)
+
+
+    def add_source(self, resource, path):
+        self.api.add_resource(resource, path)
+
+
+MainApi(app).add_source(Holi, '/holi')
+app.run()
