ced10dfe503a5cb30d41d2ad9e7f7348da30a3db
Pablo.Fuentes <pablo.fuentes@intelygez.com>
2018-11-18 17:18:43 +0100

    fix: explain container

--- container/__init__.py
+++ container/__init__.py
@@ -1,13 +1,15 @@
+# Import clients
 from clients.Db import Db
 
 db = Db()
 
+# Import repositories
 from repositories.PostsRepository import PostRepository
 
 postsRepository = PostRepository(db)
 
+# Import aplications
 from Aplication import GetPosts, CreatePost, EditPost, GetOnePost, RemovePost
-
 
 getPosts = GetPosts.GetPosts(postsRepository)
 getOnePost = GetOnePost.GetOnePost(postsRepository)
@@ -15,7 +17,7 @@ createPosts = CreatePost.CreatePost(postsRepository)
 editPost = EditPost.EditPost(postsRepository)
 removePost = RemovePost.RemovePost(postsRepository)
 
-
+# Import server instance
 from http_instance.Server import app
 from http_instance.api import MainApi
 
