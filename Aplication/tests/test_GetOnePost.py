8d0562561a6ba761bbedba8258c2a80bc43dab08
Pablo.Fuentes <pablo.fuentes@intelygez.com>
2018-11-18 19:04:31 +0100

    fix: add pytest as dependency and create firs unit tests

---
+++ Aplication/tests/test_GetOnePost.py
@@ -0,0 +1,32 @@
+from Aplication.GetOnePost import GetOnePost
+
+test_post = {'id': 'test_id',
+             'content': 'test_content',
+             'author': 'test_author'}
+
+
+class Repo:
+    def get_one(self, id):
+        return test_post
+
+    def __str__(self):
+        return 'test_repo'
+
+
+class RepoReturnsUndefined:
+    def get_one(self, id):
+        return None
+
+
+repo = Repo()
+repo2 = RepoReturnsUndefined()
+
+
+def test_get_one():
+    getOnePost = GetOnePost(repo)
+    assert getOnePost.execute('test_id') == test_post
+
+
+def test_get_one_return_not_found():
+    getOnePost = GetOnePost(repo2)
+    assert getOnePost.execute('test_id') == ({'message': 'Post not found'}, 404)
