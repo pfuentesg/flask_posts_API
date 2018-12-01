fc9660656a43143bb99aecda798fc543c429b0ea
Pablo.Fuentes <pablo.fuentes@intelygez.com>
2018-12-01 19:58:13 +0100

    feat: include api as prefix in url

--- http_instance/api.py
+++ http_instance/api.py
@@ -4,8 +4,8 @@ from flask_restful import Api
 
 class MainApi():
     def __init__(self, app):
-        self.api = Api(app)
-
+        self.api = Api(app, '/api')
+        # TODO: change this literal to config
 
     def add_source(self, resource, path):
         self.api.add_resource(resource, path)
