dac5ddaf95489110507be05a223788127869302a
Pablo.Fuentes <pablo.fuentes@intelygez.com>
2018-12-06 17:58:57 +0100

    feat: add swagger template and create ui

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from Resources.Posts import Posts, Post
 from container import db, api, app, config
+from flasgger import Swagger
+import os
 
 
 def add_resources():
@@ -10,4 +12,5 @@ def add_resources():
 if __name__ == '__main__':
     db.crete_table()
     add_resources()
+    swagger = Swagger(app, template_file='../docs/swagger.yml')
     app.run(host=config.app['host'], port=config.app['port'])
